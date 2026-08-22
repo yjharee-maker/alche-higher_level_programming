98076bf7e1506a3d0004e0bdc31b083183404aeb
Yash Bhav <y.jharee@alustudent.com>
2026-08-21 22:49:54 -0700

    8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(str):
+    for c in str:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
--- python-if_else_loops_functions/README.md
+++ python-if_else_loops_functions/README.md
@@ -6,4 +6,5 @@
 5-print_comb2.py
 6-print_comb3.py
 7-islower.py
+8-uppercase.py
 
