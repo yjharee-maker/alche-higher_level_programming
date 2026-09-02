3b1b6c113f66c66da44e3c51ba54e16b6fbb2397
Yash Bhav <y.jharee@alustudent.com>
2026-09-01 22:59:17 -0700

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initialise a square with a validated size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
--- python-classes/README.md
+++ python-classes/README.md
@@ -1,3 +1,4 @@
 0-square.py
 1-square.py
+2-square.py
 
