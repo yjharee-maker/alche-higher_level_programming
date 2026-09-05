3f91a515a098bf4ace086f0853e9495635ae3d70
Yash Bhav <y.jharee@alustudent.com>
2026-09-04 23:48:31 -0700

    4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Defines a rectangle."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialise a rectangle with a width and a height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the current rectangle area."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the current rectangle perimeter."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__width + self.__width + self.__height + self.__height
+
+    def __str__(self):
+        """Return the rectangle representation using the # character."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Return a string representation of the rectangle."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
--- python-more_classes/README.md
+++ python-more_classes/README.md
@@ -2,4 +2,5 @@
 1-rectangle.py
 2-rectangle.py
 3-rectangle.py
+4-rectangle.py
 
