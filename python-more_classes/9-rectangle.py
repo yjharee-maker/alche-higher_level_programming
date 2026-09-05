3e36024dcf9ffd2bd3a034f5435cfc4041526be5
Yash Bhav <y.jharee@alustudent.com>
2026-09-05 01:07:14 -0700

    9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -80,7 +80,7 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-        
+    
     @classmethod
     def square(cls, size=0):
         """Return a rectangle where width is equal to height."""
