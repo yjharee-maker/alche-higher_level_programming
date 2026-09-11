a4f18dd47e40553b53d251e175608aedf372cd07
Yash Bhav <y.jharee@alustudent.com>
2026-09-11 11:58:33 -0700

    3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Checking the type of an object."""
+
+
+def is_kind_of_class(obj, a_class):
+    """If object isinstance of or inherites from a_class, return True."""
+
+    return isinstance(obj, a_class)
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,4 +1,5 @@
 0-lookup.py
 1-my_list.py
 2-is_same_class.py
+3-is_kind_of_class.py
 
