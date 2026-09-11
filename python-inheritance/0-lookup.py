ed564d8bf0ab70475ac95023f403daa1c491ece1
Yash Bhav <y.jharee@alustudent.com>
2026-09-11 10:08:50 -0700

    0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Looking up attributes and methods of an object."""
+
+
 def lookup(obj):
     """Returns the list of attributes and methods available to an object."""
     return dir(obj)
