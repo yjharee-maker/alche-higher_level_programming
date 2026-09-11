b171262ca39d0abdf5a41b54105133748b3ae2d4
Yash Bhav <y.jharee@alustudent.com>
2026-09-11 12:16:55 -0700

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define BaseGeometry class."""
+
+
+class BaseGeometry():
+    """Represent BaseGeometry class."""
+
+    def area(self):
+        """Calculate the area of the BaseGeometry."""
+        raise Exception("area() is not implemented")
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -4,4 +4,5 @@
 3-is_kind_of_class.py
 4-inherits_from.py
 5-base_geometry.py
+6-base_geometry.py
 
