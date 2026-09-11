0ddb48c297ab4238048d9721d8808e81d3d56f56
Yash Bhav <y.jharee@alustudent.com>
2026-09-11 12:30:15 -0700

    7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define BaseGeometry class."""
+
+
+class BaseGeometry():
+    """Represent BaseGeometry class."""
+
+    def area(self):
+        """Calculate the area of the BaseGeometry."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate value to be a positive integer."""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -5,4 +5,5 @@
 4-inherits_from.py
 5-base_geometry.py
 6-base_geometry.py
+7-base_geometry.py
 
