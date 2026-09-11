b3a817be894361ee41cc135586adf3ca3e40a8c0
Yash Bhav <y.jharee@alustudent.com>
2026-09-11 12:07:47 -0700

    5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Create an empty class BaseGeometry."""
+
+
+class BaseGeometry():
+    pass
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -3,4 +3,5 @@
 2-is_same_class.py
 3-is_kind_of_class.py
 4-inherits_from.py
+5-base_geometry.py
 
