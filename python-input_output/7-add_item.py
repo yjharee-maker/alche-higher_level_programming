c4667a54b33cbac608fbea8944f60617d76c9171
Yash Bhav <y.jharee@alustudent.com>
2026-09-12 22:40:53 -0700

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,12 +6,18 @@ from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
+"""Name of file."""
 filename = "add_item.json"
 
+"""Load existing list from JSON file."""
 try:
     items = load_from_json_file(filename)
 except FileNotFoundError:
+    """If filename does not exist, return an empty list."""
     items = []
 
-items.extend(sys, argv[1:])
+"""Add command line arguments except the script name."""
+items.extend(sys.argv[1:])
+
+"""Save the updated list to a JSON file."""
 save_to_json_file(items, filename)
