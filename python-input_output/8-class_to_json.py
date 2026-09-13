aaedbc02d5d526acece02a55e78ed9148eba5186
Yash Bhav <y.jharee@alustudent.com>
2026-09-13 01:23:01 -0700

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Return the dictionary description with a simple data structure for JSON serialisation of an object."""
+"""Return the dictionary description for JSON serialisation of an object."""
+
 
 def class_to_json(obj):
     return obj.__dict__
