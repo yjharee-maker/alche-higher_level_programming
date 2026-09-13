81b05c03429f96f4b2a6574c52bcacdba71b1b3e
Yash Bhav <y.jharee@alustudent.com>
2026-09-12 22:33:21 -0700

    7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Add command line arguments to a JSON file."""
+
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys, argv[1:])
+save_to_json_file(items, filename)
