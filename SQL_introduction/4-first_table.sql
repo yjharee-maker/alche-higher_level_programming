5ee5598b1670f2ef02de307b023c8e91df4cbbe5
Yash Bhav <y.jharee@alustudent.com>
2026-09-18 10:42:50 -0700

    4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- Creates a table with fields id and name in the current database.
+CREATE TABLE IF NOT EXISTS first_table(
+	id INT;
+	name VARCHAR(256)
+);
--- SQL_introduction/README.md
+++ SQL_introduction/README.md
@@ -2,4 +2,5 @@
 1-create_database_if_missing.sql
 2-remove_database.sql
 3-list_tables.sql
+4-first_table.sql
 
