7bd65c5611388b0696bffec769dad09330ebd62d
yjharee-maker <y.jharee@alustudent.com>
2026-09-26 06:05:28 +0000

    4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request with the required header and display the response body
-curl -s --header "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
