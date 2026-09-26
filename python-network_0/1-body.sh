be06a43254abbb836248f18615c8858c742654cb
yjharee-maker <y.jharee@alustudent.com>
2026-09-26 05:40:01 +0000

    1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display the body if response code is 200
-curl -s -w '%{http_code}' "$1" | grep -q 200 && curl -s "$1"
+curl -s -o /tmp/body -w '%{http_code}' "$1" | grep -q '^200$' && cat /tmp/body
