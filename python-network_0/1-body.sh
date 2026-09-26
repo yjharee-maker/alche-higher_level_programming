db8237ae6a400467634216de2de8a25f76f8b1af
yjharee-maker <y.jharee@alustudent.com>
2026-09-26 05:34:32 +0000

    1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Display the body if response code is 200
 status=$(curl -s -o /tmp/curl_body -w '%{http_code}' "$1")
-if [ "$status" = "200" ]; then
-	cat /tmp/curl_body
-fi
+if [ "$status" = "200" ]; then cat /tmp/curl_body fi
